691a8ce768e07d696559f655dee0c0d50823b249
JunHatanaka <nextseason6@gmail.com>
2022-03-09 22:38:23 +0900

    Fix: 記事の順番を直す

--- node/builder.js
+++ node/builder.js
@@ -1,20 +1,18 @@
 // これを実行するとブログ記事がindex.mdに追記される
-// ビルド先のブログ記事をrmで削除するものと、これの実行をまとめたshellscriptを作る
-// 記事にタイムスタンプを入れたりとか工夫する
-// セットアップコマンドとか展開してQiitaかZennにあげよう。
+// readFile, appendFileだと記事の順番おかしくなるので同期処理メソッドを使用。
+// 参考：https://jsprimer.net/use-case/nodecli/read-file/
 const fs = require('fs');
 const path = "./posts/"
 
 fs.readdir(path, (err, files) => {
   const filesR = files.reverse()
-    for (let index = 1; index < filesR.length; index++) {
+    for (let index = 0; index < filesR.length-1; index++) {
         const element = filesR[index];
-        fs.readFile(path+element, { encoding: "utf8" }, (err, file) => {
-            fs.appendFile("./docs/index.md", file+"\n", (err) =>{
-                if (err) throw err;
-                console.log("書き込みOK！");
-            })
-        });
+        const file = fs.readFileSync(path+element, { encoding: "utf8" }, (err, file) => {});
+        fs.appendFileSync("./docs/index.md", file+"\n"+element.replace(".md","")+"\n"+"<hr>"+"\n", (err) =>{
+            if (err) throw err;
+            console.log("書き込みOK！");
+        })
     }
 });
 
