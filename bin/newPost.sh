d1cc3e5f5acee2116aa5d742fd983d8aedfc7e1b
JunHatanaka <nextseason6@gmail.com>
2022-07-03 05:10:04 +0900

    Use autoPush.sh

--- bin/newPost.sh
+++ bin/newPost.sh
@@ -2,12 +2,11 @@
 touch posts/`date "+%Y-%m-%d-%H%M%S"`.md
 echo ""
 echo ""
-echo "↓↓↓↓↓↓↓↓↓↓↓↓新ファイルpath↓↓↓↓↓↓↓↓↓↓↓↓↓↓"
-echo "↓↓↓↓↓↓↓↓↓↓↓↓新ファイルpath↓↓↓↓↓↓↓↓↓↓↓↓↓↓"
-echo "↓↓↓↓↓↓↓↓↓↓↓↓新ファイルpath↓↓↓↓↓↓↓↓↓↓↓↓↓↓"
 echo posts/`date "+%Y-%m-%d-%H%M%S"`.md
-echo "↑↑↑↑↑↑↑↑↑↑↑↑新ファイルpath↑↑↑↑↑↑↑↑↑↑↑↑↑↑"
-echo "↑↑↑↑↑↑↑↑↑↑↑↑新ファイルpath↑↑↑↑↑↑↑↑↑↑↑↑↑↑"
-echo "↑↑↑↑↑↑↑↑↑↑↑↑新ファイルpath↑↑↑↑↑↑↑↑↑↑↑↑↑↑"
+echo posts/`date "+%Y-%m-%d-%H%M%S"`.md
+echo posts/`date "+%Y-%m-%d-%H%M%S"`.md
+echo posts/`date "+%Y-%m-%d-%H%M%S"`.md
+echo posts/`date "+%Y-%m-%d-%H%M%S"`.md
+echo posts/`date "+%Y-%m-%d-%H%M%S"`.md
 echo ""
 echo ""
